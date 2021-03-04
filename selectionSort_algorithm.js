bc4ec730eedcf8b3ccd3de6007fd708644342c54
Edur <eduru@me.com>
2021-03-04 14:57:50 +0900

    improving the algorithm, if i is equal to the lowest value we skip the swap step

--- selectionSort_algorithm.js
+++ selectionSort_algorithm.js
@@ -15,13 +15,11 @@ const selectionSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     let lowest = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[lowest]) {
-        lowest = j;
-      }
-      swap(arr, i, lowest);
+      if (arr[j] < arr[lowest]) lowest = j;
+      if (i !== lowest) swap(arr, i, lowest);
     }
   }
   return arr;
 };
 
-console.log(selectionSort([2, 4, 5, 3, 1]));
+console.log(selectionSort([20, 54, 105, 300, 1]));
